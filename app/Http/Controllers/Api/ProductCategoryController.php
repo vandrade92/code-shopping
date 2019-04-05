059cb859a8e3b5ddff42b0a566f7137f939d5412
vandrade92 <vagner.matias@gmail.com>
2019-04-05 15:19:15 -0300

    Criando serializador para relacionamento

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -7,6 +7,7 @@ use CodeShopping\Http\Controllers\Controller;
 use CodeShopping\Models\Product;
 use CodeShopping\Models\Category;
 use CodeShopping\Http\Requests\ProductCategoryRequest;
+use CodeShopping\Http\Resources\ProductCategoryResource;
 
 
 class ProductCategoryController extends Controller
@@ -14,7 +15,7 @@ class ProductCategoryController extends Controller
 
     public function index(Product $product)
     {
-         return $product->categories;
+         return new ProductCategoryResource($product);
     }
 
     public function store(ProductCategoryRequest $request, Product $product)
@@ -22,13 +23,13 @@ class ProductCategoryController extends Controller
          $changed = $product->categories()->sync($request->categories);
          $categoriesAttachedId = $changed['attached'];
          $categories = Category::whereIn('id', $categoriesAttachedId)->get();
-         return $categories->count() ? response()->json($categories, 201) : [];
+         return $categories->count() ? response()->json(new ProductCategoryResource($product), 201) : [];
     }
 
     public function destroy(Product $product, Category $category)
     {
         $product->categories()->detach($category->id);
-        
+
         return response()->json([],204);
     }
 }
