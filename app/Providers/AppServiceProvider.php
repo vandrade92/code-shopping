a6bcc4bdb39621c24346c4897cc40e96d55f8592
vandrade92 <vagner.matias@gmail.com>
2019-04-08 10:22:44 -0300

    Orientação por eventos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace CodeShopping\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use CodeShopping\Models\ProductInput;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        ProductInput::created(function($input){
+             $product = $input->product;
+             $product->stock += $input->amount;
+             $product->save();
+        });
     }
 
     /**
--- database/seeds/ProductInputsTableSeeder.php
+++ database/seeds/ProductInputsTableSeeder.php
@@ -21,8 +21,6 @@ class ProductInputsTableSeeder extends Seeder
                $product = $products->random();
                $input->product_id = $product->id;
                $input->save();
-               $product->stock += $input->amount;
-               $product->save();
         });
     }
 }
