d69a7b92efafc536131c6a24f1d02ba2ad605290
vandrade92 <vagner.matias@gmail.com>
2019-04-09 09:58:42 -0300

    Gerando nome da imagem do upload

--- database/seeds/ProductPhotosTableSeeder.php
+++ database/seeds/ProductPhotosTableSeeder.php
@@ -1,11 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Seeder;
 use CodeShopping\Models\Product;
 use CodeShopping\Models\ProductPhoto;
+use Illuminate\Support\Collection;
+use Illuminate\Http\UploadedFile;
 
 class ProductPhotosTableSeeder extends Seeder
 {
+     private $allFakerPhotos;
+     private $fakerPhotosPath = 'app/faker/product_photos';
     /**
      * Run the database seeds.
      *
@@ -13,6 +19,7 @@ class ProductPhotosTableSeeder extends Seeder
      */
     public function run()
     {
+        $this->allFakerPhotos = $this->getFakerPhotos();
         $products = Product::all();
         $this->deleteAllPhotosInProductsPath();
         $self  = $this;
@@ -21,6 +28,12 @@ class ProductPhotosTableSeeder extends Seeder
              $self->createPhotosModels($product);
         });
 
+    }
+
+    private function getFakerPhotos() : Collection
+    {
+         $path = storage_path($this->fakerPhotosPath);
+         return collect(\File::allFiles($path));
     }
 
     private function deleteAllPhotosInProductsPath(){
@@ -44,9 +57,27 @@ class ProductPhotosTableSeeder extends Seeder
 
     private function createPhotoModel(Product $product)
     {
-         ProductPhoto::create([
+        $photo = ProductPhoto::create([
               'product_id' => $product->id,
               'file_name' => 'imagem.jpg'
          ]);
+         $this->generatePhoto($photo);
+    }
+
+    private function generatePhoto(ProductPhoto $photo)
+    {
+         $photo->file_name = $this->uploadPhoto($photo->product_id);
+         $photo->save();
+    }
+
+    private function uploadPhoto($productId) : string
+    {
+         $photoFile = $this->allFakerPhotos->random();
+         $uploadFile = new UploadedFile(
+              $photoFile->getRealPath(),
+              str_random(16).'.' . $photoFile->getExtension()
+         );
+
+         return $uploadFile->hashName();
     }
 }
