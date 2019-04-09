67b16f77f5d51fbd07ad72cf684ab2631eda5109
vandrade92 <vagner.matias@gmail.com>
2019-04-09 08:59:25 -0300

    Criando registros das imagens

--- app/Models/ProductPhoto.php
+++ app/Models/ProductPhoto.php
@@ -6,9 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductPhoto extends Model
 {
-     const BASE_PATCH = 'app/public';
+     const BASE_PATH = 'app/public';
      const DIR_PRODUCTS = 'products';
-     const PRODUCT_PATCH = self::BASE_PATCH . '/' . self::DIR_PRODUCTS;
+     const PRODUCTS_PATH = self::BASE_PATH . '/' . self::DIR_PRODUCTS;
 
      protected $fillable =['file_name', 'product_id'];
+
+     public static function photosPath($productId)
+     {
+
+          $path = self::PRODUCTS_PATH;
+
+          return storage_path("{$path}/{$productId}");
+     }
 }
--- database/seeds/ProductPhotosTableSeeder.php
+++ database/seeds/ProductPhotosTableSeeder.php
@@ -15,14 +15,38 @@ class ProductPhotosTableSeeder extends Seeder
     {
         $products = Product::all();
         $this->deleteAllPhotosInProductsPath();
-        $products->each(function($product){
-
+        $self  = $this;
+        $products->each(function($product) use($self){
+             $self->createPhotoDir($product);
+             $self->createPhotosModels($product);
         });
 
     }
 
     private function deleteAllPhotosInProductsPath(){
-         $path = ProductPhoto::PRODUCT_PATCH;
+         $path = ProductPhoto::PRODUCTS_PATH;
          \File::deleteDirectory(storage_path($path), true);
+    }
+
+    private function createPhotoDir(Product $product)
+    {
+         $path = ProductPhoto::photosPath($product->id);
+         \File::makeDirectory($path, 0777, true);
+    }
+
+    private function createPhotosModels(Product $product)
+    {
+         foreach(range(1,5) as $v)
+         {
+           $this->createPhotoModel($product);
+         }
+    }
+
+    private function createPhotoModel(Product $product)
+    {
+         ProductPhoto::create([
+              'product_id' => $product->id,
+              'file_name' => 'imagem.jpg'
+         ]);
     }
 }
