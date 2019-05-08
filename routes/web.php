fa79f944c3fb4e76516e983d12af17d5c4a5726f
vandrade92 <vagner.matias@gmail.com>
2019-05-08 11:02:15 -0300

    Testando envio de e-mail de recuperação de senha

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// Route::get('password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm')->name('password.reset');
+// Route::post('password/reset', 'Auth\ForgotPasswordController@reset');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
