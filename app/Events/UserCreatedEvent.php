d9277dcf54390ad862d511a9f214af0953e4a4af
vandrade92 <vagner.matias@gmail.com>
2019-05-08 10:59:56 -0300

    Configurando evento personalizado quando usuário é criado

---
+++ app/Events/UserCreatedEvent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CodeShopping\Events;
+
+use CodeShopping\Models\User;
+
+class UserCreatedEvent
+{
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser(): User
+    {
+         return $this->user;
+    }
+
+}
