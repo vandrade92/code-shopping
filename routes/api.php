b2183e95dc01d42b15244d48beeb3057d9f1352a
vandrade92 <vagner.matias@gmail.com>
2019-05-13 14:41:58 -0300

    Auto refresh token

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::group(['namespace' => 'Api', 'as' => 'api.'], function(){
      Route::name('login')->post('login', 'AuthController@login');
      Route::name('refresh')->post('refresh', 'AuthController@refresh');
 
-     Route::group(['middleware' => 'auth:api'], function(){
+     Route::group(['middleware' => ['auth:api','jwt.refresh']], function(){
           Route::name('logout')->post('logout', 'AuthController@logout');
           Route::name('me')->get('me', 'AuthController@me');
           Route::patch('products/{product}/restore', 'ProductController@restore');
@@ -32,6 +32,5 @@ Route::group(['namespace' => 'Api', 'as' => 'api.'], function(){
           Route::resource('inputs', 'ProductInputController', ['only' => ['index', 'store', 'show']]);
           Route::resource('outputs', 'ProductOutputController', ['only' => ['index', 'store', 'show']]);
           Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
-
      });
 });
