c5cc549607fa30c18feb4b3df3090c9af4f3224c
vandrade92 <vagner.matias@gmail.com>
2019-05-14 11:39:23 -0300

    Entendendo o componente login

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { LoginComponent } from './components/pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     LoginComponent
   ],
   imports: [
