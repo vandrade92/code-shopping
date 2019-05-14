3f9ce257bca38b53b295e0318b41f10b253a200a
vandrade92 <vagner.matias@gmail.com>
2019-05-14 11:58:23 -0300

    Interpolação

--- _angular-app/src/app/components/pages/login/login.component.ts
+++ _angular-app/src/app/components/pages/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email = 'admin@user.com';
+  obj = {
+    email: 'outroemail.com',
+     array: [1]
+  };
+
   constructor() { }
 
   ngOnInit() {
+
+    setTimeout( ()=> {
+      this.email = 'qualquer coisa';
+    }, 3000)
   }
 
 }
