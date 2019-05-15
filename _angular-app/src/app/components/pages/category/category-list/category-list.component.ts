b6aa9c0277735e2b8730b59d2be1fc7428a80cd2
vandrade92 <vagner.matias@gmail.com>
2019-05-15 10:46:19 -0300

    Consumindo categorias no componente de listagem

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories = [];
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    this.http.get<Array<any>>('http://localhost:8000/api/categories', {
+            headers: {
+              Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU1NzkyNzc5NywiZXhwIjoxNTU3OTMxMzk3LCJuYmYiOjE1NTc5Mjc3OTcsImp0aSI6IjhHMUFzUW9pR25SOGlzd2IiLCJzdWIiOjEsInBydiI6IjBkZjM4NTQ4ZDU1YmE1ZDMyNTE5ZDgxOGUwZDlhMWU4Y2NkYjVhOGMiLCJlbWFpbCI6ImFkbWluQHVzZXIuY29tIiwibmFtZSI6Ik1yLiBDYXNleSBTcG9yZXIgViJ9.KBt7BEFpi6KY_Ue-o7X9zOOaFsLyO3fqz82giWkEdiw`
+            }
+          })
+              .subscribe(data => this.categories = data);
   }
 
 }
