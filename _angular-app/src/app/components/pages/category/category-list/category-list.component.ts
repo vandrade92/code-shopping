70c00149a45e2579f585ccec5dff001bcecc36c1
vandrade92 <vagner.matias@gmail.com>
2019-05-15 11:11:24 -0300

    Guardando token no localstorage

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -16,9 +16,10 @@ export class CategoryListComponent implements OnInit {
 
   ngOnInit() {
     console.log('ngOnInit');
+    const token = window.localStorage.getItem('token');
     this.http.get<Array<any>>('http://localhost:8000/api/categories', {
             headers: {
-              Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU1NzkyNzc5NywiZXhwIjoxNTU3OTMxMzk3LCJuYmYiOjE1NTc5Mjc3OTcsImp0aSI6IjhHMUFzUW9pR25SOGlzd2IiLCJzdWIiOjEsInBydiI6IjBkZjM4NTQ4ZDU1YmE1ZDMyNTE5ZDgxOGUwZDlhMWU4Y2NkYjVhOGMiLCJlbWFpbCI6ImFkbWluQHVzZXIuY29tIiwibmFtZSI6Ik1yLiBDYXNleSBTcG9yZXIgViJ9.KBt7BEFpi6KY_Ue-o7X9zOOaFsLyO3fqz82giWkEdiw`
+              Authorization: `Bearer ${token}`
             }
           })
               .subscribe(data => this.categories = data);
