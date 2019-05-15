bc177d7e071c018ddc7e15bf7756e37c37b79636
vandrade92 <vagner.matias@gmail.com>
2019-05-15 10:23:29 -0300

    Iniciando com Single Page Application

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -1,22 +1,39 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
+import { CategoryListComponent } from './components/pages/category/category-list/category-list.component';
+
+// Routes
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  {
+    path: 'categories/list', component: CategoryListComponent
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
